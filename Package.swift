f2d7140537d7799dad796b4b6344218f39d30501
martinmitrevski <martinmitrevski.oh@gmail.com>
2021-11-18 16:41:34 +0100

    updated package file

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
     targets: [
         .target(
             name: "StreamChatSwiftUI",
-            dependencies: ["stream-chat-swift", "Nuke", "SwiftyGif", "NukeUI"],
+            dependencies: ["StreamChat", "Nuke", "SwiftyGif", "NukeUI"],
             exclude: ["README.md", "Info.plist", "Generated/L10n_template.stencil"],
             resources: [.process("Resources")]
         )
