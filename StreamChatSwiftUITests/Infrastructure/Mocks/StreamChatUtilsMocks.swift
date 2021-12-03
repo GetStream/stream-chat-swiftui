48a58db7477483d8b9a1f44329d343638aa8214b
martinmitrevski <martinmitrevski.oh@gmail.com>
2021-12-03 12:09:36 +0100

    added tests for injecting utils

---
+++ StreamChatSwiftUITests/Infrastructure/Mocks/StreamChatUtilsMocks.swift
@@ -0,0 +1,34 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import StreamChatSwiftUI
+import UIKit
+
+/// Mock implementation of `VideoPreviewLoader`.
+class VideoPreviewLoader_Mock: VideoPreviewLoader {
+    
+    var loadPreviewVideoCalled = false
+    
+    func loadPreviewForVideo(at url: URL, completion: @escaping (Result<UIImage, Error>) -> Void) {
+        loadPreviewVideoCalled = true
+    }    
+    
+}
+
+/// Mock implementation of `ImageLoading`.
+class ImageLoaderUtils_Mock: ImageLoading {
+    
+    var loadImageCalled = false
+    
+    func loadImage(
+        url: URL?,
+        imageCDN: ImageCDN,
+        resize: Bool,
+        preferredSize: CGSize?,
+        completion: @escaping ((Result<UIImage, Error>) -> Void)
+    ) {
+        loadImageCalled = true
+    }
+
+}
---
+++ StreamChatSwiftUITests/Tests/Utils/StreamChat_Utils_Tests.swift
@@ -0,0 +1,60 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+@testable import StreamChat
+@testable import StreamChatSwiftUI
+import XCTest
+
+class StreamChat_Utils_Tests: XCTestCase {
+    
+    private let testURL = URL(string: "https://example.com")!
+    
+    var streamChat: StreamChat!
+    
+    @Injected(\.utils) var utils
+    
+    private var chatClient: ChatClient = {
+        let client = ChatClient.mock()
+        client.currentUserId = .unique
+        return client
+    }()
+    
+    override func setUp() {
+        let utils = Utils(
+            videoPreviewLoader: VideoPreviewLoader_Mock(),
+            imageLoader: ImageLoaderUtils_Mock()
+        )
+        streamChat = StreamChat(chatClient: chatClient, utils: utils)
+    }
+
+    func test_streamChatUtils_injectVideoPreviewLoader() {
+        // Given
+        let videoPreviewLoader = utils.videoPreviewLoader as! VideoPreviewLoader_Mock
+        
+        // When
+        videoPreviewLoader.loadPreviewForVideo(at: testURL, completion: { _ in })
+        
+        // Then
+        XCTAssert(videoPreviewLoader.loadPreviewVideoCalled == true)
+    }
+    
+    func test_streamChatUtils_injectImageLoader() {
+        // Given
+        let imageLoader = utils.imageLoader as! ImageLoaderUtils_Mock
+        
+        // When
+        imageLoader.loadImage(
+            url: testURL,
+            imageCDN: utils.imageCDN,
+            resize: true,
+            preferredSize: nil,
+            completion: {_ in }
+        )
+        
+        // Then
+        XCTAssert(imageLoader.loadImageCalled == true)
+    }
+    
+}
