9c577d3d6f96fd731977b507e4666468404c9668
martinmitrevski <martinmitrevski.oh@gmail.com>
2021-12-03 15:56:09 +0100

    added tests for MessageActionsViewModel

---
+++ StreamChatSwiftUITests/Tests/ChatChannel/MessageActionsViewModel_Tests.swift
@@ -0,0 +1,50 @@
+//
+// Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import XCTest
+@testable import StreamChatSwiftUI
+@testable import StreamChat
+
+import XCTest
+
+class MessageActionsViewModel_Tests: XCTestCase {
+    
+    private var chatClient: ChatClient = {
+        let client = ChatClient.mock()
+        client.currentUserId = .unique
+        return client
+    }()
+    
+    private var streamChat: StreamChat?
+        
+    override func setUp() {
+        super.setUp()
+        streamChat = StreamChat(chatClient: chatClient)
+    }
+
+    func test_messageActionsViewModel_confirmationAlertShown() {
+        // Given
+        let actions = MessageAction.defaultActions(
+            for: .mock(
+                id: .unique,
+                cid: .unique,
+                text: "test",
+                author: .mock(id: .unique)
+            ),
+            chatClient: chatClient,
+            onDismiss: {},
+            onError: { _ in }
+        )
+        let viewModel = MessageActionsViewModel(messageActions: actions)
+        let action = actions[0]
+        
+        // When
+        viewModel.alertAction = action
+        
+        // Then
+        XCTAssert(action.confirmationPopup != nil)
+        XCTAssert(viewModel.alertShown == true)
+    }
+
+}
