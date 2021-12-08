3c1becb6338fc4ea52b7e1500c07ddc3384677ef
martinmitrevski <martinmitrevski.oh@gmail.com>
2021-12-08 09:52:26 +0100

    added snapshot tests

--- StreamChatSwiftUITests/Tests/ChatChannel/MessageView_Tests.swift
+++ StreamChatSwiftUITests/Tests/ChatChannel/MessageView_Tests.swift
@@ -67,4 +67,96 @@ class MessageView_Tests: XCTestCase {
         // Then
         assertSnapshot(matching: view, as: .image)
     }
+    
+    func test_messageViewGiphy_snapshot() {
+        // Given
+        let giphyMessage = ChatMessage.mock(
+            id: .unique,
+            cid: .unique,
+            text: "",
+            author: .mock(id: .unique),
+            attachments: ChatChannelTestHelpers.giphyAttachments
+        )
+        
+        // When
+        let view = MessageView(
+            factory: DefaultViewFactory.shared,
+            message: giphyMessage,
+            contentWidth: defaultScreenSize.width,
+            isFirst: true
+        )
+        .frame(width: defaultScreenSize.width, height: defaultScreenSize.height)
+
+        // Then
+        assertSnapshot(matching: view, as: .image)
+    }
+    
+    func test_messageViewLink_snapshot() {
+        // Given
+        let linkMessage = ChatMessage.mock(
+            id: .unique,
+            cid: .unique,
+            text: "",
+            author: .mock(id: .unique),
+            attachments: ChatChannelTestHelpers.linkAttachments
+        )
+        
+        // When
+        let view = MessageView(
+            factory: DefaultViewFactory.shared,
+            message: linkMessage,
+            contentWidth: defaultScreenSize.width,
+            isFirst: true
+        )
+        .frame(width: defaultScreenSize.width, height: defaultScreenSize.height)
+
+        // Then
+        assertSnapshot(matching: view, as: .image)
+    }
+    
+    func test_messageViewVideo_snapshot() {
+        // Given
+        let videoMessage = ChatMessage.mock(
+            id: .unique,
+            cid: .unique,
+            text: "",
+            author: .mock(id: .unique),
+            attachments: ChatChannelTestHelpers.videoAttachments
+        )
+        
+        // When
+        let view = MessageView(
+            factory: DefaultViewFactory.shared,
+            message: videoMessage,
+            contentWidth: defaultScreenSize.width,
+            isFirst: true
+        )
+        .frame(width: defaultScreenSize.width, height: defaultScreenSize.height)
+
+        // Then
+        assertSnapshot(matching: view, as: .image)
+    }
+    
+    func test_messageViewFile_snapshot() {
+        // Given
+        let fileMessage = ChatMessage.mock(
+            id: .unique,
+            cid: .unique,
+            text: "",
+            author: .mock(id: .unique),
+            attachments: ChatChannelTestHelpers.fileAttachments
+        )
+        
+        // When
+        let view = MessageView(
+            factory: DefaultViewFactory.shared,
+            message: fileMessage,
+            contentWidth: defaultScreenSize.width,
+            isFirst: true
+        )
+        .frame(width: defaultScreenSize.width, height: defaultScreenSize.height)
+
+        // Then
+        assertSnapshot(matching: view, as: .image)
+    }
 }
