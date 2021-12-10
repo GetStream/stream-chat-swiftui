e210f44ed155d0040f354ab80074913ea717ea40
martinmitrevski <martinmitrevski.oh@gmail.com>
2021-12-10 14:21:07 +0100

    fixed reactions redraw

--- Sources/StreamChatSwiftUI/ChatChannel/ChatChannelViewModel.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/ChatChannelViewModel.swift
@@ -269,7 +269,11 @@ extension ChatMessage: Identifiable {
     
     var reactionScoresId: String {
         var output = ""
-        for (key, score) in reactionScores {
+        let sorted = reactionScores.keys.sorted { type1, type2 in
+            type1.id > type2.id
+        }
+        for key in sorted {
+            let score = reactionScores[key] ?? 0
             output += "\(key.rawValue)\(score)"
         }
         return output
