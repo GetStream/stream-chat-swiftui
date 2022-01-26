85dcfd48a96b7362b2ace2b96feaa16763e9ddac
martinmitrevski <martinmitrevski.oh@gmail.com>
2022-01-26 10:53:49 +0100

    Added possibility to disable scrolling in the channel list

--- Sources/StreamChatSwiftUI/ChatChannelList/ChatChannelList.swift
+++ Sources/StreamChatSwiftUI/ChatChannelList/ChatChannelList.swift
@@ -12,6 +12,7 @@ public struct ChannelList<Factory: ViewFactory>: View {
     var channels: LazyCachedMapCollection<ChatChannel>
     @Binding var selectedChannel: ChatChannel?
     @Binding var currentChannelId: String?
+    private var scrollable: Bool
     private var onlineIndicatorShown: (ChatChannel) -> Bool
     private var imageLoader: (ChatChannel) -> UIImage
     private var onItemTap: (ChatChannel) -> Void
@@ -23,6 +24,86 @@ public struct ChannelList<Factory: ViewFactory>: View {
     private var leadingSwipeButtonTapped: (ChatChannel) -> Void
     
     public init(
+        factory: Factory,
+        channels: LazyCachedMapCollection<ChatChannel>,
+        selectedChannel: Binding<ChatChannel?>,
+        currentChannelId: Binding<String?>,
+        scrollable: Bool = true,
+        onlineIndicatorShown: @escaping (ChatChannel) -> Bool,
+        imageLoader: @escaping (ChatChannel) -> UIImage,
+        onItemTap: @escaping (ChatChannel) -> Void,
+        onItemAppear: @escaping (Int) -> Void,
+        channelNaming: @escaping (ChatChannel) -> String,
+        channelDestination: @escaping (ChatChannel) -> Factory.ChannelDestination,
+        trailingSwipeRightButtonTapped: @escaping (ChatChannel) -> Void,
+        trailingSwipeLeftButtonTapped: @escaping (ChatChannel) -> Void,
+        leadingSwipeButtonTapped: @escaping (ChatChannel) -> Void
+    ) {
+        self.factory = factory
+        self.channels = channels
+        self.onItemTap = onItemTap
+        self.onItemAppear = onItemAppear
+        self.channelNaming = channelNaming
+        self.channelDestination = channelDestination
+        self.imageLoader = imageLoader
+        self.onlineIndicatorShown = onlineIndicatorShown
+        self.trailingSwipeRightButtonTapped = trailingSwipeRightButtonTapped
+        self.trailingSwipeLeftButtonTapped = trailingSwipeLeftButtonTapped
+        self.leadingSwipeButtonTapped = leadingSwipeButtonTapped
+        self.scrollable = scrollable
+        _selectedChannel = selectedChannel
+        _currentChannelId = currentChannelId
+    }
+    
+    public var body: some View {
+        Group {
+            if scrollable {
+                ScrollView {
+                    channelsVStack
+                }
+            } else {
+                channelsVStack
+            }
+        }
+    }
+    
+    private var channelsVStack: some View {
+        ChannelsLazyVStack(
+            factory: factory,
+            channels: channels,
+            selectedChannel: $selectedChannel,
+            currentChannelId: $currentChannelId,
+            onlineIndicatorShown: onlineIndicatorShown,
+            imageLoader: imageLoader,
+            onItemTap: onItemTap,
+            onItemAppear: onItemAppear,
+            channelNaming: channelNaming,
+            channelDestination: channelDestination,
+            trailingSwipeRightButtonTapped: trailingSwipeRightButtonTapped,
+            trailingSwipeLeftButtonTapped: trailingSwipeLeftButtonTapped,
+            leadingSwipeButtonTapped: leadingSwipeButtonTapped
+        )
+    }
+}
+
+/// LazyVStack displaying list of channels.
+struct ChannelsLazyVStack<Factory: ViewFactory>: View {
+    
+    private var factory: Factory
+    var channels: LazyCachedMapCollection<ChatChannel>
+    @Binding var selectedChannel: ChatChannel?
+    @Binding var currentChannelId: String?
+    private var onlineIndicatorShown: (ChatChannel) -> Bool
+    private var imageLoader: (ChatChannel) -> UIImage
+    private var onItemTap: (ChatChannel) -> Void
+    private var onItemAppear: (Int) -> Void
+    private var channelNaming: (ChatChannel) -> String
+    private var channelDestination: (ChatChannel) -> Factory.ChannelDestination
+    private var trailingSwipeRightButtonTapped: (ChatChannel) -> Void
+    private var trailingSwipeLeftButtonTapped: (ChatChannel) -> Void
+    private var leadingSwipeButtonTapped: (ChatChannel) -> Void
+    
+    init(
         factory: Factory,
         channels: LazyCachedMapCollection<ChatChannel>,
         selectedChannel: Binding<ChatChannel?>,
@@ -53,33 +134,31 @@ public struct ChannelList<Factory: ViewFactory>: View {
     }
     
     public var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 0) {
-                ForEach(channels) { channel in
-                    factory.makeChannelListItem(
-                        channel: channel,
-                        channelName: channelNaming(channel),
-                        avatar: imageLoader(channel),
-                        onlineIndicatorShown: onlineIndicatorShown(channel),
-                        disabled: currentChannelId == channel.id,
-                        selectedChannel: $selectedChannel,
-                        swipedChannelId: $currentChannelId,
-                        channelDestination: channelDestination,
-                        onItemTap: onItemTap,
-                        trailingSwipeRightButtonTapped: trailingSwipeRightButtonTapped,
-                        trailingSwipeLeftButtonTapped: trailingSwipeLeftButtonTapped,
-                        leadingSwipeButtonTapped: leadingSwipeButtonTapped
-                    )
-                    .onAppear {
-                        if let index = channels.firstIndex(where: { chatChannel in
-                            chatChannel.id == channel.id
-                        }) {
-                            onItemAppear(index)
-                        }
+        LazyVStack(spacing: 0) {
+            ForEach(channels) { channel in
+                factory.makeChannelListItem(
+                    channel: channel,
+                    channelName: channelNaming(channel),
+                    avatar: imageLoader(channel),
+                    onlineIndicatorShown: onlineIndicatorShown(channel),
+                    disabled: currentChannelId == channel.id,
+                    selectedChannel: $selectedChannel,
+                    swipedChannelId: $currentChannelId,
+                    channelDestination: channelDestination,
+                    onItemTap: onItemTap,
+                    trailingSwipeRightButtonTapped: trailingSwipeRightButtonTapped,
+                    trailingSwipeLeftButtonTapped: trailingSwipeLeftButtonTapped,
+                    leadingSwipeButtonTapped: leadingSwipeButtonTapped
+                )
+                .onAppear {
+                    if let index = channels.firstIndex(where: { chatChannel in
+                        chatChannel.id == channel.id
+                    }) {
+                        onItemAppear(index)
                     }
-                    
-                    Divider()
                 }
+                
+                Divider()
             }
         }
     }
