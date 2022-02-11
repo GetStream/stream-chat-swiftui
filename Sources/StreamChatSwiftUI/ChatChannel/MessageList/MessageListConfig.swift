a8fe907e4fc9c7a8fdee11ebd28738840b458d3b
martinmitrevski <martinmitrevski.oh@gmail.com>
2022-02-11 11:29:56 +0100

    Added config for showing / hiding message helper views

--- Sources/StreamChatSwiftUI/ChatChannel/MessageList/MessageListConfig.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/MessageList/MessageListConfig.swift
@@ -2,6 +2,7 @@
 // Copyright © 2022 Stream.io Inc. All rights reserved.
 //
 
+import CoreGraphics
 import StreamChat
 
 /// Configuration for the message list.
@@ -9,16 +10,46 @@ public struct MessageListConfig {
     public init(
         messageListType: MessageListType = .messaging,
         typingIndicatorPlacement: TypingIndicatorPlacement = .bottomOverlay,
-        groupMessages: Bool = true
+        groupMessages: Bool = true,
+        showAvatars: Bool = false,
+        messageDisplayOptions: MessageDisplayOptions = MessageDisplayOptions(),
+        messagePaddings: MessagePaddings = MessagePaddings()
     ) {
         self.messageListType = messageListType
         self.typingIndicatorPlacement = typingIndicatorPlacement
         self.groupMessages = groupMessages
+        self.messageDisplayOptions = messageDisplayOptions
+        self.messagePaddings = messagePaddings
     }
     
     let messageListType: MessageListType
     let typingIndicatorPlacement: TypingIndicatorPlacement
     let groupMessages: Bool
+    let messageDisplayOptions: MessageDisplayOptions
+    let messagePaddings: MessagePaddings
+}
+
+/// Contains information about the message paddings.
+public struct MessagePaddings {
+    
+    /// Horizontal padding for messages.
+    let horizontal: CGFloat
+    
+    public init(horizontal: CGFloat = 8) {
+        self.horizontal = horizontal
+    }
+}
+
+/// Used to show and hide different helper views around the message.
+public struct MessageDisplayOptions {
+    
+    let showAvatars: Bool
+    let showMessageDate: Bool
+    
+    public init(showAvatars: Bool = true, showMessageDate: Bool = true) {
+        self.showAvatars = showAvatars
+        self.showMessageDate = showMessageDate
+    }
 }
 
 /// Type of message list. Currently only `messaging` is supported.
