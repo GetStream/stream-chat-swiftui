7eeb5d3e890890e6852214af4156c8271c1ab2b8
martinmitrevski <martinmitrevski.oh@gmail.com>
2022-04-06 12:06:04 +0200

    Disconnecting chat client on logout

--- DemoAppSwiftUI/CustomChannelHeader.swift
+++ DemoAppSwiftUI/CustomChannelHeader.swift
@@ -78,6 +78,7 @@ struct CustomChannelModifier: ChannelListHeaderViewModifier {
                     message: Text("Are you sure you want to sign out?"),
                     primaryButton: .destructive(Text("Sign out")) {
                         withAnimation {
+                            chatClient.disconnect()
                             AppState.shared.userState = .notLoggedIn
                         }
                     },
