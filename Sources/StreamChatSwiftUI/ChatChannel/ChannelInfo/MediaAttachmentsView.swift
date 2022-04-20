9845d38e612590ac6aba9afa67cb39bb6e21686a
martinmitrevski <martinmitrevski.oh@gmail.com>
2022-04-20 09:15:58 +0200

    initial implementation of media files in chat info

--- Sources/StreamChatSwiftUI/ChatChannel/ChannelInfo/MediaAttachmentsView.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/ChannelInfo/MediaAttachmentsView.swift
@@ -9,6 +9,18 @@ struct MediaAttachmentsView: View {
     
     @StateObject private var viewModel: MediaAttachmentsViewModel
     
+    private static let spacing: CGFloat = 2
+    
+    private static var itemWidth: CGFloat {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return (UIScreen.main.bounds.size.width / 3) - spacing * 3
+        } else {
+            return 120
+        }
+    }
+    
+    private let columns = [GridItem(.adaptive(minimum: itemWidth), spacing: spacing)]
+    
     init(channel: ChatChannel) {
         _viewModel = StateObject(
             wrappedValue: MediaAttachmentsViewModel(channel: channel)
@@ -16,6 +28,23 @@ struct MediaAttachmentsView: View {
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 2) {
+                ForEach(viewModel.mediaItems) { mediaItem in
+                    LazyLoadingImage(
+                        source: mediaItem.imageURL,
+                        width: Self.itemWidth,
+                        height: Self.itemWidth
+                    )
+                    .frame(
+                        width: Self.itemWidth,
+                        height: Self.itemWidth
+                    )
+                    .clipped()
+                }
+            }
+            .padding(.horizontal, 2)
+            .animation(nil)
+        }
     }
 }
--- Sources/StreamChatSwiftUI/ChatChannel/ChannelInfo/MediaAttachmentsViewModel.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/ChannelInfo/MediaAttachmentsViewModel.swift
@@ -8,9 +8,64 @@ import SwiftUI
 
 class MediaAttachmentsViewModel: ObservableObject {
     
+    @Published var mediaItems = [MediaItem]()
+    @Published var loading = false
+    
+    @Injected(\.chatClient) var chatClient
+    
     private let channel: ChatChannel
+    private var messageSearchController: ChatMessageSearchController!
     
     init(channel: ChatChannel) {
         self.channel = channel
+        messageSearchController = chatClient.messageSearchController()
+        loadMessages()
     }
+    
+    private func loadMessages() {
+        let query = MessageSearchQuery(
+            channelFilter: .equal(.cid, to: channel.cid),
+            messageFilter: .withAttachments([.image, .video])
+        )
+        
+        loading = true
+        messageSearchController.search(query: query, completion: { [weak self] _ in
+            guard let self = self else { return }
+            self.updateAttachments()
+            self.loading = false
+        })
+    }
+    
+    private func updateAttachments() {
+        var result = [MediaItem]()
+        for message in messageSearchController.messages {
+            let imageAttachments = message.imageAttachments
+            let videoAttachments = message.videoAttachments
+            for imageAttachment in imageAttachments {
+                let mediaItem = MediaItem(
+                    id: imageAttachment.id.rawValue,
+                    imageURL: imageAttachment.imageURL,
+                    isVideo: false
+                )
+                result.append(mediaItem)
+            }
+            for videoAttachment in videoAttachments {
+                let mediaItem = MediaItem(
+                    id: videoAttachment.id.rawValue,
+                    imageURL: videoAttachment.videoURL,
+                    isVideo: true
+                )
+                result.append(mediaItem)
+            }
+        }
+        withAnimation {
+            self.mediaItems = result
+        }
+    }
+}
+
+struct MediaItem: Identifiable {
+    let id: String
+    let imageURL: URL
+    let isVideo: Bool
 }
