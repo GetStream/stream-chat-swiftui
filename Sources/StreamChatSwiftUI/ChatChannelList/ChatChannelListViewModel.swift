7fd95c64a540785dca79764a38e00af4752d9c50
martinmitrevski <martinmitrevski.oh@gmail.com>
2022-05-02 20:49:45 +0200

    Reverted keyboard animation fix

--- Sources/StreamChatSwiftUI/ChatChannelList/ChatChannelListViewModel.swift
+++ Sources/StreamChatSwiftUI/ChatChannelList/ChatChannelListViewModel.swift
@@ -224,9 +224,6 @@ open class ChatChannelListViewModel: ObservableObject, ChatChannelListController
         let newValue = numberOfScreens > 1
         if newValue != hideTabBar {
             hideTabBar = newValue
-            if !hideTabBar {
-                selectedChannel = nil
-            }
         }
     }
     
