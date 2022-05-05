3c102a1dc1b04934663d3fb5ac528ed5f10a9ec8
martinmitrevski <martinmitrevski.oh@gmail.com>
2022-05-05 17:12:14 +0200

    Added tests for channel header

---
+++ StreamChatSwiftUITests/Tests/ChatChannel/ChatChannelHeader_Tests.swift
@@ -0,0 +1,58 @@
+//
+// Copyright © 2022 Stream.io Inc. All rights reserved.
+//
+
+import SnapshotTesting
+@testable import StreamChat
+@testable import StreamChatSwiftUI
+import SwiftUI
+import XCTest
+
+class ChatChannelHeader_Tests: StreamChatTestCase {
+
+    func test_chatChannelHeaderModifier_snapshot() {
+        // Given
+        let channel = ChatChannel.mockDMChannel(name: "Test channel")
+        
+        // When
+        let view = NavigationView {
+            Text("Test")
+                .applyDefaultSize()
+                .modifier(DefaultChannelHeaderModifier(channel: channel))
+        }
+        .applyDefaultSize()
+
+        // Then
+        assertSnapshot(matching: view, as: .image)
+    }
+
+    func test_chatChannelHeader_snapshot() {
+        // Given
+        let channel = ChatChannel.mockDMChannel(name: "Test channel")
+        
+        // When
+        let view = NavigationView {
+            Text("Test")
+                .applyDefaultSize()
+                .toolbar {
+                    DefaultChatChannelHeader(channel: channel, headerImage: .circleImage)
+                }
+        }
+        .applyDefaultSize()
+
+        // Then
+        assertSnapshot(matching: view, as: .image)
+    }
+    
+    func test_channelTitleView_snapshot() {
+        // Given
+        let channel = ChatChannel.mockDMChannel(name: "Test channel")
+        
+        // When
+        let view = ChannelTitleView(channel: channel, shouldShowTypingIndicator: true)
+            .applyDefaultSize()
+        
+        // Then
+        assertSnapshot(matching: view, as: .image)
+    }
+}
