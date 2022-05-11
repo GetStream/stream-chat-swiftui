9f245ac5d82753570c4ecd617934b8d19d9addf7
Adam Rush <adam.rush@getstream.io>
2022-05-11 10:38:57 +0200

    Adding Sentry tracking to the demo app

--- DemoAppSwiftUI/AppDelegate.swift
+++ DemoAppSwiftUI/AppDelegate.swift
@@ -6,6 +6,7 @@ import StreamChat
 import StreamChatSwiftUI
 import SwiftUI
 import UIKit
+import Sentry
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     
@@ -52,6 +53,14 @@ class AppDelegate: NSObject, UIApplicationDelegate {
          
          streamChat = StreamChat(chatClient: chatClient, utils: utils)
          */
+        
+        #if RELEASE
+        // We're tracking Crash Reports / Issues from the Demo App to keep improving the SDK
+        SentrySDK.start { options in
+                options.dsn = "https://00e1d4e0213541d2b103ea252b77f120@o389650.ingest.sentry.io/6395489"
+                options.tracesSampleRate = 1.0
+        }
+        #endif
         
         streamChat = StreamChat(chatClient: chatClient)
         
