623980b9dcfd1df0a809bdda3adcf5142af24d97
martinmitrevski <martinmitrevski.oh@gmail.com>
2022-05-30 11:03:33 +0200

    Updated date formatter locale

--- StreamChatSwiftUITests/Tests/ChatChannel/ChatChannelViewDateOverlay_Tests.swift
+++ StreamChatSwiftUITests/Tests/ChatChannel/ChatChannelViewDateOverlay_Tests.swift
@@ -18,7 +18,17 @@ class ChatChannelViewDateOverlay_Tests: StreamChatTestCase {
             df.locale = .init(identifier: "en_US")
             return df
         }()
-        let utils = Utils(messageListConfig: MessageListConfig(dateIndicatorPlacement: .messageList))
+        let dateFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .none
+            formatter.timeStyle = .short
+            formatter.locale = .init(identifier: "en_US")
+            return formatter
+        }()
+        let utils = Utils(
+            dateFormatter: dateFormatter,
+            messageListConfig: MessageListConfig(dateIndicatorPlacement: .messageList)
+        )
         streamChat = StreamChat(chatClient: chatClient, utils: utils)
     }
 
