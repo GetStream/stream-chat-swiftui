e80c6f0ac61dbf06a53c024bc3c9349edeb377a1
martinmitrevski <martinmitrevski.oh@gmail.com>
2022-05-31 10:35:51 +0200

    Added time interval config for grouping messages

--- Sources/StreamChatSwiftUI/ChatChannel/ChatChannelViewModel.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/ChatChannelViewModel.swift
@@ -345,23 +345,34 @@ open class ChatChannelViewModel: ObservableObject, MessagesDataSource {
     
     private func groupMessages() {
         var temp = [String: [String]]()
+        let primary = "primary"
         for (index, message) in messages.enumerated() {
-            let dateString = messagesDateFormatter.string(from: message.createdAt)
-            let prefix = messageCachingUtils.authorId(for: message)
-            let key = "\(prefix)-\(dateString)"
-            if temp[key] == nil {
-                temp[key] = [message.id]
-            } else {
-                // check if the previous message is not sent by the same user.
-                let previousIndex = index - 1
-                if previousIndex >= 0 {
-                    let previous = messages[previousIndex]
-                    let previousAuthorId = messageCachingUtils.authorId(for: previous)
-                    let shouldAddKey = prefix != previousAuthorId
-                    if shouldAddKey {
-                        temp[key]?.append(message.id)
-                    }
-                }
+            let date = message.createdAt
+            if index == 0 {
+                temp[message.id] = [primary]
+                continue
+            }
+
+            let previous = index - 1
+            let previousMessage = messages[previous]
+            let currentAuthorId = messageCachingUtils.authorId(for: message)
+            let previousAuthorId = messageCachingUtils.authorId(for: previousMessage)
+
+            if currentAuthorId != previousAuthorId {
+                temp[message.id] = [primary]
+            }
+
+            if previousMessage.type == .error
+                || previousMessage.type == .ephemeral
+                || previousMessage.type == .system {
+                temp[message.id] = [primary]
+                continue
+            }
+
+            let delay = previousMessage.createdAt.timeIntervalSince(date)
+
+            if delay > utils.messageListConfig.maxTimeIntervalBetweenMessagesInGroup {
+                temp[message.id] = [primary]
             }
         }
         
