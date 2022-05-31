27db12829c3478263a298f1740c5443a1133128e
Pol Quintana <pol.quintana1@gmail.com>
2022-05-31 19:23:59 +0400

    Add token class to dynamically determine when SwiftUI SDK is used

---
+++ Sources/StreamChatSwiftUI/SDKIdentifier.swift
@@ -0,0 +1,11 @@
+//
+//  SDKIdentifier.swift
+//  StreamChatSwiftUI
+//
+//  Created by Pol Quintana on 31/5/22.
+//
+
+import Foundation
+
+/// Used to dynamically find which frameworks are linked to an app using NSClassFromString
+class SDKIdentifier: NSObject {}
