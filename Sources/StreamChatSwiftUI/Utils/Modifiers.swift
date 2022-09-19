c9226ad0d026b38da6f92d8d2159bed2a8fd1897
saidReclip <said@reclip.app>
2022-09-19 17:31:56 +0300

    make ShadowModifier public

--- Sources/StreamChatSwiftUI/Utils/Modifiers.swift
+++ Sources/StreamChatSwiftUI/Utils/Modifiers.swift
@@ -25,8 +25,8 @@ struct ShadowViewModifier: ViewModifier {
 }
 
 /// Modifier for adding shadow to a view.
-struct ShadowModifier: ViewModifier {
-    func body(content: Content) -> some View {
+public struct ShadowModifier: ViewModifier {
+    public func body(content: Content) -> some View {
         content
             .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 12)
             .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
