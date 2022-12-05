756346c5f15ca89d07e0108b7e8ef534ef5f5e25
saidReclip <said@reclip.app>
2022-12-05 19:35:04 +0300

    bring lazy back

--- Sources/StreamChatSwiftUI/ChatChannelList/ChatChannelList.swift
+++ Sources/StreamChatSwiftUI/ChatChannelList/ChatChannelList.swift
@@ -144,7 +144,7 @@ struct ChannelsLazyVStack<Factory: ViewFactory>: View {
     }
     
     public var body: some View {
-        VStack(spacing: 0) {
+        LazyVStack(spacing: 0) {
             ForEach(channels) { channel in
                 factory.makeChannelListItem(
                     channel: channel,
