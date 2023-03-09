598595e82d719b3a48084bf624cf4610af4aa1dd
martinmitrevski <martinmitrevski.oh@gmail.com>
2023-03-09 22:34:00 +0100

    LazyImage uses image CDN request

--- Sources/StreamChatSwiftUI/Utils/LazyImageExtensions.swift
+++ Sources/StreamChatSwiftUI/Utils/LazyImageExtensions.swift
@@ -9,18 +9,40 @@ import SwiftUI
 extension LazyImage {
 
     public init(imageURL: URL?) where Content == NukeUI.Image {
+        let imageCDN = InjectedValues[\.utils].imageCDN
+        guard let imageURL = imageURL else {
+            #if COCOAPODS
+            self.init(source: imageURL)
+            #else
+            self.init(url: imageURL, resizingMode: .aspectFill)
+            #endif
+            return
+        }
+        let urlRequest = imageCDN.urlRequest(forImage: imageURL)
+        let imageRequest = ImageRequest(urlRequest: urlRequest)
         #if COCOAPODS
-        self.init(source: imageURL)
+        self.init(source: imageRequest)
         #else
-        self.init(url: imageURL, resizingMode: .aspectFill)
+        self.init(request: imageRequest, resizingMode: .aspectFill)
         #endif
     }
 
     public init(imageURL: URL?, @ViewBuilder content: @escaping (LazyImageState) -> Content) {
+        let imageCDN = InjectedValues[\.utils].imageCDN
+        guard let imageURL = imageURL else {
+            #if COCOAPODS
+            self.init(source: imageURL, content: content)
+            #else
+            self.init(url: imageURL, content: content)
+            #endif
+            return
+        }
+        let urlRequest = imageCDN.urlRequest(forImage: imageURL)
+        let imageRequest = ImageRequest(urlRequest: urlRequest)
         #if COCOAPODS
-        self.init(source: imageURL, content: content)
+        self.init(source: imageRequest, content: content)
         #else
-        self.init(url: imageURL, content: content)
+        self.init(request: imageRequest, content: content)
         #endif
     }
 }
