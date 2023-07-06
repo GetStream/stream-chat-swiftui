38f7f2b22561104cf9ab691f19f82e0ffa9394e4
martinmitrevski <martinmitrevski.oh@gmail.com>
2023-07-06 17:49:44 +0200

    Updated StreamChat dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "4.33.0"),        
+        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "4.34.0"),        
         .package(url: "https://github.com/kean/Nuke.git", .exact("11.3.1"))
     ],
     targets: [
