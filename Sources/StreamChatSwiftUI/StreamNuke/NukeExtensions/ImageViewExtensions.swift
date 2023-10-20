e05614399827f494c3e3a4fae9dd4c91fa2b9d9f
Alexey Alter-Pesotskiy <a.alterpesotskiy@mail.ru>
2023-10-20 10:29:47 +0100

    Fix one more warning

--- Sources/StreamChatSwiftUI/StreamNuke/NukeExtensions/ImageViewExtensions.swift
+++ Sources/StreamChatSwiftUI/StreamNuke/NukeExtensions/ImageViewExtensions.swift
@@ -172,11 +172,16 @@ private final class ImageViewController {
 
     // Lazily create a controller for a given view and associate it with a view.
     static func controller(for view: ImageDisplayingView) -> ImageViewController {
-        if let controller = objc_getAssociatedObject(view, &ImageViewController.controllerAK) as? ImageViewController {
+        if let controller = withUnsafePointer(to: &ImageViewController.controllerAK, { keyPointer in
+            objc_getAssociatedObject(view, keyPointer) as? ImageViewController
+        }) {
             return controller
         }
+        
         let controller = ImageViewController(view: view)
-        objc_setAssociatedObject(view, &ImageViewController.controllerAK, controller, .OBJC_ASSOCIATION_RETAIN)
+        withUnsafePointer(to: &ImageViewController.controllerAK) { keyPointer in
+            objc_setAssociatedObject(view, keyPointer, controller, .OBJC_ASSOCIATION_RETAIN)
+        }
         return controller
     }
 
