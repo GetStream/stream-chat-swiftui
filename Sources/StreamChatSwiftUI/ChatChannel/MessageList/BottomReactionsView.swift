485b6a054a4a95c5b46e333fcda599eba2896f38
martinmitrevski <martinmitrevski.oh@gmail.com>
2023-10-25 17:54:09 +0200

    Added support for multiple lines reactions

--- Sources/StreamChatSwiftUI/ChatChannel/MessageList/BottomReactionsView.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/MessageList/BottomReactionsView.swift
@@ -12,6 +12,7 @@ struct BottomReactionsView: View {
     @Injected(\.colors) var colors
     
     var showsAllInfo: Bool
+    var reactionsPerRow: Int
     var onTap: () -> ()
     var onLongPress: () -> ()
     
@@ -20,17 +21,47 @@ struct BottomReactionsView: View {
     init(
         message: ChatMessage,
         showsAllInfo: Bool,
+        reactionsPerRow: Int = 4,
         onTap: @escaping () -> (),
         onLongPress: @escaping () -> ()
     ) {
         self.showsAllInfo = showsAllInfo
         self.onTap = onTap
+        self.reactionsPerRow = reactionsPerRow
         self.onLongPress = onLongPress
         _viewModel = StateObject(wrappedValue: ReactionsOverlayViewModel(message: message))
     }
     
     var body: some View {
-        HStack {
+        if reactions.count > 3 {
+            let numberOfRows = Int((Double(reactions.count + 1) / Double(reactionsPerRow)).rounded(.up))
+            VStack {
+                ForEach(0..<numberOfRows, id: \.self) { row in
+                    let start = row * reactionsPerRow
+                    let end = start + (reactionsPerRow - 1) >= reactions.count ? reactions.count - 1 : start + (reactionsPerRow - 1)
+                    let slice = end < start ? [] : Array(reactions[start...end])
+                    let isEndRow = slice.isEmpty ? true : end == (reactions.count - 1)
+                    HStack {
+                        if message.isRightAligned {
+                            Spacer()
+                        }
+                        content(for: slice, isEndRow: isEndRow)
+                        if !message.isRightAligned {
+                            Spacer()
+                        }
+                    }
+                }
+            }
+        } else {
+            HStack {
+                content(for: reactions)
+            }
+            .offset(y: -2)
+        }
+    }
+    
+    private func content(for reactions: [MessageReactionType], isEndRow: Bool = true) -> some View {
+        Group {
             ForEach(reactions) { reaction in
                 if let image = ReactionsIconProvider.icon(for: reaction, useLargeIcons: false) {
                     HStack(spacing: 4) {
@@ -48,7 +79,7 @@ struct BottomReactionsView: View {
                     .background(Color(colors.background1))
                     .modifier(
                         BubbleModifier(
-                            corners: corners(for: reaction),
+                            corners: corners(for: reaction, in: reactions, isEndRow: isEndRow),
                             backgroundColors: [Color(colors.background1)],
                             cornerRadius: 16
                         )
@@ -61,33 +92,43 @@ struct BottomReactionsView: View {
                     }
                 }
             }
-            Button(
-                action: onTap,
-                label: {
-                    Image(systemName: "face.smiling.inverse")
-                        .padding(.all, 8)
-                        .modifier(
-                            BubbleModifier(
-                                corners: (message.isSentByCurrentUser && showsAllInfo) ? [.bottomLeft, .bottomRight, .topLeft] : .allCorners,
-                                backgroundColors: [Color(colors.background1)],
-                                cornerRadius: 16
+            
+            if isEndRow && reactions.count < reactionsPerRow {
+                Button(
+                    action: onTap,
+                    label: {
+                        Image(systemName: "face.smiling.inverse")
+                            .padding(.all, 8)
+                            .modifier(
+                                BubbleModifier(
+                                    corners: (message.isSentByCurrentUser && showsAllInfo) ? [.bottomLeft, .bottomRight, .topLeft] : .allCorners,
+                                    backgroundColors: [Color(colors.background1)],
+                                    cornerRadius: 16
+                                )
                             )
-                        )
-                }
-            )
+                    }
+                )
+            }
         }
-        .offset(y: -2)
     }
     
     private var message: ChatMessage {
         viewModel.message
     }
     
-    private func corners(for reaction: MessageReactionType) -> UIRectCorner {
+    private func corners(
+        for reaction: MessageReactionType,
+        in reactions: [MessageReactionType],
+        isEndRow: Bool
+    ) -> UIRectCorner {
         if message.isSentByCurrentUser || reaction != reactions.first || !showsAllInfo {
             return .allCorners
         }
-        return [.bottomLeft, .bottomRight, .topRight]
+        if isEndRow {
+            return [.bottomLeft, .bottomRight, .topRight]
+        } else {
+            return .allCorners
+        }
     }
     
     private var reactions: [MessageReactionType] {
