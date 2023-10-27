d8b4991742bd327adfddfccda613944fc308f598
martinmitrevski <martinmitrevski.oh@gmail.com>
2023-10-27 12:49:43 +0200

    Added more tests

--- StreamChatSwiftUITests/Tests/ChatChannel/BottomReactionsView_Tests.swift
+++ StreamChatSwiftUITests/Tests/ChatChannel/BottomReactionsView_Tests.swift
@@ -110,4 +110,61 @@ final class BottomReactionsView_Tests: StreamChatTestCase {
         // Then
         assertSnapshot(matching: view, as: .image(perceptualPrecision: 0.95))
     }
+    
+    func test_bottomReactions_multipleRows_twoPerRow() {
+        // Given
+        let reactionScores: [MessageReactionType: Int] = [
+            .init(rawValue: "like") : 2,
+            .init(rawValue: "love") : 1,
+            .init(rawValue: "haha") : 1,
+            .init(rawValue: "sad") : 1,
+            .init(rawValue: "wow") : 10
+        ]
+        let message = ChatMessage.mock(
+            reactionScores: reactionScores,
+            currentUserReactions: [.mock(type: .init(rawValue: "like"))]
+        )
+        
+        // When
+        let view = BottomReactionsView(
+            message: message,
+            showsAllInfo: true,
+            reactionsPerRow: 2,
+            onTap: {},
+            onLongPress: {}
+        )
+        .padding()
+        .frame(width: 200)
+        
+        // Then
+        assertSnapshot(matching: view, as: .image(perceptualPrecision: 0.95))
+    }
+    
+    func test_bottomReactions_multipleRows_buttonNewRow() {
+        // Given
+        let reactionScores: [MessageReactionType: Int] = [
+            .init(rawValue: "like") : 2,
+            .init(rawValue: "love") : 1,
+            .init(rawValue: "haha") : 1,
+            .init(rawValue: "sad") : 1
+        ]
+        let message = ChatMessage.mock(
+            reactionScores: reactionScores,
+            currentUserReactions: [.mock(type: .init(rawValue: "like"))]
+        )
+        
+        // When
+        let view = BottomReactionsView(
+            message: message,
+            showsAllInfo: true,
+            reactionsPerRow: 2,
+            onTap: {},
+            onLongPress: {}
+        )
+        .padding()
+        .frame(width: 200)
+        
+        // Then
+        assertSnapshot(matching: view, as: .image(perceptualPrecision: 0.95))
+    }
 }
