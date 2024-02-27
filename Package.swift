a1e4ee03b9350fd7e57dec28501a87cefca251b0
martinmitrevski <martinmitrevski.oh@gmail.com>
2024-02-28 00:00:29 +0100

    Updated StreamChat dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "4.48.0"),
+        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "4.49.0"),
     ],
     targets: [
         .target(
