d44fd45ddc4f30526da7e2d90558abfb0c58dc4c
Alexey Alter-Pesotskiy <a.alterpesotskiy@mail.ru>
2024-03-08 14:51:10 +0000

    Update things

--- StreamChatSwiftUITestsAppTests/Tests/Performance/MessageListScrollTime.swift
+++ StreamChatSwiftUITestsAppTests/Tests/Performance/MessageListScrollTime.swift
@@ -6,18 +6,11 @@ import XCTest
 
 @available(iOS 15.0, *)
 class MessageListScrollTime: StreamTestCase {
-    
-    // FIXME
-    override func setUpWithError() throws {
-        mockServerEnabled = false
-        try super.setUpWithError()
-    }
 
     func testMessageListScrollTime() {
         WHEN("user opens the message list") {
-            // FIXME
-            // backendRobot.generateChannels(count: 1, messagesCount: 100, withAttachments: true)
-            // participantRobot.addReaction(type: .like)
+             backendRobot.generateChannels(count: 1, messagesCount: 100, withAttachments: true)
+             participantRobot.addReaction(type: .like)
             userRobot.login().openChannel()
         }
         THEN("user scrolls the message list") {
