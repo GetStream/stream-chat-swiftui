d572cb1a6e9830a2502dcc7ab5317e3e2b7a9446
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2024-07-10 11:43:02 +0200

    Expose waveform properties as open (#539)

--- Sources/StreamChatSwiftUI/ChatChannel/MessageList/AsyncVoiceMessages/WaveformView.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/MessageList/AsyncVoiceMessages/WaveformView.swift
@@ -12,8 +12,8 @@ open class WaveformView: UIView {
     
     @Injected(\.images) var images
     
-    var onSliderChanged: ((TimeInterval) -> Void)?
-    var onSliderTapped: (() -> Void)?
+    open var onSliderChanged: ((TimeInterval) -> Void)?
+    open var onSliderTapped: (() -> Void)?
     
     public struct Content: Equatable {
         /// When set to `true` the waveform will be updating with the data live (scrolling to the trailing side
@@ -49,7 +49,7 @@ open class WaveformView: UIView {
         }
     }
 
-    var content: Content = .initial {
+    open var content: Content = .initial {
         didSet { updateContent() }
     }
     
@@ -69,10 +69,10 @@ open class WaveformView: UIView {
 
     // MARK: - UI Components
 
-    open private(set) lazy var audioVisualizationView: AudioVisualizationView = .init()
+    open lazy var audioVisualizationView: AudioVisualizationView = .init()
         .withoutAutoresizingMaskConstraints
 
-    open private(set) lazy var slider: UISlider = .init()
+    open lazy var slider: UISlider = .init()
         .withoutAutoresizingMaskConstraints
 
     // MARK: - UI Lifecycle
