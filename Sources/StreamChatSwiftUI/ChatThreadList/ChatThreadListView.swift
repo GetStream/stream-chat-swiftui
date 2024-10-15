13343a07f992b339f78889a2b7f33a4533f52a5f
Nuno Vieira <nuno.fcvieira93@gmail.com>
2024-10-15 17:15:27 +0100

    Remove unused properties in Thread List View

--- Sources/StreamChatSwiftUI/ChatThreadList/ChatThreadListView.swift
+++ Sources/StreamChatSwiftUI/ChatThreadList/ChatThreadListView.swift
@@ -15,9 +15,7 @@ public struct ChatThreadListView<Factory: ViewFactory>: View {
 
     private let viewFactory: Factory
     private let title: String
-    private let customOnItemTap: ((ChatThread) -> Void)?
     private var embedInNavigationView: Bool
-    private var handleTabBarVisibility: Bool
 
     /// Creates a thread list view.
     ///
@@ -26,8 +24,6 @@ public struct ChatThreadListView<Factory: ViewFactory>: View {
     ///   - viewModel: The view model instance providing the data. Default view model is created if nil.
     ///   - threadListController: The thread list controller managing the list of threads used as a data source for the view model. Default controller is created if nil.
     ///   - title: A custom title used as the navigation bar title.
-    ///   - onItemTap: A closure for handling a tap on the thread item. Default closure updates the ``ChatThreadListViewModel/selectedThread`` property in the view model.
-    ///   - handleTabBarVisibility: True, if TabBar visibility should be automatically updated.
     ///   - embedInNavigationView: True, if the thread list view should be embedded in a navigation stack.
     ///
     /// Changing the instance of the passed in `viewModel` or `threadListController` does not have an effect without reloading the thread list view by assigning a custom identity. The custom identity should be refreshed when either of the passed in instances have been recreated.
@@ -42,8 +38,6 @@ public struct ChatThreadListView<Factory: ViewFactory>: View {
         viewModel: ChatThreadListViewModel? = nil,
         threadListController: ChatThreadListController? = nil,
         title: String? = nil,
-        onItemTap: ((ChatThread) -> Void)? = nil,
-        handleTabBarVisibility: Bool = true,
         embedInNavigationView: Bool = true
     ) {
         _viewModel = StateObject(
@@ -53,9 +47,7 @@ public struct ChatThreadListView<Factory: ViewFactory>: View {
         )
         self.viewFactory = viewFactory
         self.title = title ?? L10n.Thread.title
-        self.handleTabBarVisibility = handleTabBarVisibility
         self.embedInNavigationView = embedInNavigationView
-        customOnItemTap = onItemTap
     }
 
     public var body: some View {
