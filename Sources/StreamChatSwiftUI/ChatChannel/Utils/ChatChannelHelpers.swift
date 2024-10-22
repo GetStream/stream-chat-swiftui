561be6ff2ff2eb3c9d24d189c86a27f584ad19eb
Nuno Vieira <nuno.fcvieira93@gmail.com>
2024-10-22 10:30:35 +0100

    Fix crash when opening message overlay in iPad with a TabBar (#627)

--- Sources/StreamChatSwiftUI/ChatChannel/Utils/ChatChannelHelpers.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/Utils/ChatChannelHelpers.swift
@@ -82,6 +82,9 @@ public struct BottomLeftView<Content: View>: View {
 
 /// Returns the top most view controller.
 func topVC() -> UIViewController? {
+    // TODO: Refactor ReactionsOverlayView to use a background blur, instead of a snapshot.
+    /// Since the current approach is too error-prone and dependent of the app's hierarchy,
+
     let keyWindow = UIApplication.shared.windows.filter { $0.isKeyWindow }.first
 
     if var topController = keyWindow?.rootViewController {
@@ -92,10 +95,16 @@ func topVC() -> UIViewController? {
         if UIDevice.current.userInterfaceIdiom == .pad {
             let children = topController.children
             if !children.isEmpty {
-                let splitVC = children[0]
-                let sideVCs = splitVC.children
-                if sideVCs.count > 1 {
-                    topController = sideVCs[1]
+                if let splitVC = children[0] as? UISplitViewController,
+                   let contentVC = splitVC.viewControllers.last {
+                    topController = contentVC
+                    return topController
+                } else if let tabVC = children[0] as? UITabBarController,
+                          let selectedVC = tabVC.selectedViewController {
+                    // If the selectedVC is split view, we need to grab the content view of it
+                    // other wise, the selectedVC is already the content view.
+                    let selectedContentVC = selectedVC.children.first?.children.last?.children.first
+                    topController = selectedContentVC ?? selectedVC
                     return topController
                 }
             }
