f566ec12608971b4ae36353bc86cf74e62c4ebd6
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-01-13 16:50:46 +0000

    Fix reactions users view not paginating results (#712)
    * Fix reactions users view not paginating results
    
    * Update CHANGELOG.md
    
    * Create seperate file for the view model

--- Sources/StreamChatSwiftUI/ChatChannel/Reactions/ReactionsUsersView.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/Reactions/ReactionsUsersView.swift
@@ -7,46 +7,48 @@ import SwiftUI
 
 /// View displaying users who have reacted to a message.
 struct ReactionsUsersView: View {
-
+    @StateObject private var viewModel: ReactionsUsersViewModel
+    
     @Injected(\.fonts) private var fonts
     @Injected(\.colors) private var colors
 
-    var message: ChatMessage
     var maxHeight: CGFloat
 
     private static let columnCount = 4
     private static let itemSize: CGFloat = 64
 
     private let columns = Array(
-        repeating:
-        GridItem(
-            .adaptive(minimum: itemSize),
-            alignment: .top
-        ),
+        repeating: GridItem(.adaptive(minimum: itemSize), alignment: .top),
         count: columnCount
     )
+    
+    init(message: ChatMessage, maxHeight: CGFloat) {
+        self.maxHeight = maxHeight
+        _viewModel = StateObject(wrappedValue: ReactionsUsersViewModel(message: message))
+    }
 
-    private var reactions: [ChatMessageReaction] {
-        Array(message.latestReactions)
+    init(viewModel: ReactionsUsersViewModel, maxHeight: CGFloat) {
+        self.maxHeight = maxHeight
+        _viewModel = StateObject(wrappedValue: viewModel)
     }
 
     var body: some View {
         HStack {
-            if message.isRightAligned {
+            if viewModel.isRightAligned {
                 Spacer()
             }
 
             VStack(alignment: .center) {
-                Text(L10n.Reaction.Authors.numberOfReactions(reactions.count))
+                Text(L10n.Reaction.Authors.numberOfReactions(viewModel.totalReactionsCount))
                     .foregroundColor(Color(colors.text))
                     .font(fonts.title3)
                     .fontWeight(.bold)
                     .padding()
 
-                if reactions.count > Self.columnCount {
+                if viewModel.reactions.count > Self.columnCount {
                     ScrollView {
                         LazyVGrid(columns: columns, alignment: .center, spacing: 8) {
-                            ForEach(reactions) { reaction in
+                            ForEach(viewModel.reactions) { reaction in
                                 ReactionUserView(
                                     reaction: reaction,
                                     imageSize: Self.itemSize
@@ -57,7 +59,7 @@ struct ReactionsUsersView: View {
                     .frame(maxHeight: maxHeight)
                 } else {
                     HStack(alignment: .top, spacing: 0) {
-                        ForEach(reactions) { reaction in
+                        ForEach(viewModel.reactions) { reaction in
                             ReactionUserView(
                                 reaction: reaction,
                                 imageSize: Self.itemSize
@@ -70,7 +72,7 @@ struct ReactionsUsersView: View {
             .background(Color(colors.background))
             .cornerRadius(16)
 
-            if !message.isRightAligned {
+            if !viewModel.isRightAligned {
                 Spacer()
             }
         }
