d49b2ba61e9665e7831dc3684ee7318c8ca65f06
Alexey Alter-Pesotskiy <alex@testableapple.com>
2025-01-28 14:27:48 +0000

    Resolve compilation issues

--- StreamChatSwiftUITests/Infrastructure/TestTools/ChatClient_Mock.swift
+++ StreamChatSwiftUITests/Infrastructure/TestTools/ChatClient_Mock.swift
@@ -34,12 +34,7 @@ public extension ChatClient {
                 databaseContainerBuilder: {
                     DatabaseContainer_Spy(
                         kind: $0,
-                        shouldFlushOnStart: $1,
-                        shouldResetEphemeralValuesOnStart: $2,
-                        bundle: Bundle(for: StreamChatTestCase.self),
-                        localCachingSettings: $3,
-                        deletedMessagesVisibility: $4,
-                        shouldShowShadowedMessages: $5
+                        chatClientConfig: $1
                     )
                 },
                 authenticationRepositoryBuilder: AuthenticationRepository_Mock.init
