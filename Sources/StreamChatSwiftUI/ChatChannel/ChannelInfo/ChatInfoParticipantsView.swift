ac7fd364340d51c24f953e5134aee3c801493cae
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2025-03-06 18:40:01 +0100

    Customizing the message avatar view is reflected in all views that use it (#772)

--- Sources/StreamChatSwiftUI/ChatChannel/ChannelInfo/ChatInfoParticipantsView.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/ChannelInfo/ChatInfoParticipantsView.swift
@@ -6,19 +6,35 @@ import StreamChat
 import SwiftUI
 
 /// View for the chat info participants.
-struct ChatInfoParticipantsView: View {
+struct ChatInfoParticipantsView<Factory: ViewFactory>: View {
 
     @Injected(\.fonts) private var fonts
     @Injected(\.colors) private var colors
 
+    let factory: Factory
     var participants: [ParticipantInfo]
     var onItemAppear: (ParticipantInfo) -> Void
+    
+    init(
+        factory: Factory = DefaultViewFactory.shared,
+        participants: [ParticipantInfo],
+        onItemAppear: @escaping (ParticipantInfo) -> Void
+    ) {
+        self.factory = factory
+        self.participants = participants
+        self.onItemAppear = onItemAppear
+    }
 
     var body: some View {
         LazyVStack {
             ForEach(participants) { participant in
                 HStack {
-                    MessageAvatarView(avatarURL: participant.chatUser.imageURL)
+                    let displayInfo = UserDisplayInfo(
+                        id: participant.chatUser.id,
+                        name: participant.chatUser.name ?? "",
+                        imageURL: participant.chatUser.imageURL
+                    )
+                    factory.makeMessageAvatarView(for: displayInfo)
 
                     VStack(alignment: .leading, spacing: 4) {
                         Text(participant.displayName)
