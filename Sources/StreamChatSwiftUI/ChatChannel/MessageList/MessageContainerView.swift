b93c03467dcdcf8c99b15682fe26e94434e59379
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-04-15 16:05:13 +0100

    Fix being able to swipe to reply an unsent giphy

--- Sources/StreamChatSwiftUI/ChatChannel/MessageList/MessageContainerView.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/MessageList/MessageContainerView.swift
@@ -135,7 +135,7 @@ public struct MessageContainerView<Factory: ViewFactory>: View {
                             coordinateSpace: .local
                         )
                         .updating($offset) { (value, gestureState, _) in
-                            if message.isDeleted || !channel.config.repliesEnabled {
+                            if !message.isInteractionEnabled || !channel.config.repliesEnabled {
                                 return
                             }
                             // Using updating since onEnded is not called if the gesture is canceled.
