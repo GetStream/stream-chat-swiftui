12a51fd778bf9305988e713f2bde705dfb0f9793
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-05-27 19:06:42 +0100

    Add test coverage

--- StreamChatSwiftUITests/Tests/ChatChannel/MessageReadIndicatorView_Tests.swift
+++ StreamChatSwiftUITests/Tests/ChatChannel/MessageReadIndicatorView_Tests.swift
@@ -72,12 +72,38 @@ class MessageReadIndicatorView_Tests: StreamChatTestCase {
         assertSnapshot(matching: view, as: .image(perceptualPrecision: precision))
     }
 
+    func test_messageReadIndicatorView_snapshotSyncing() {
+        // Given
+        let view = MessageReadIndicatorView(
+            readUsers: [],
+            showReadCount: false,
+            localState: .syncing
+        )
+        .frame(width: 50, height: 16)
+
+        // Then
+        assertSnapshot(matching: view, as: .image(perceptualPrecision: precision))
+    }
+
     func test_messageReadIndicatorView_snapshotMessageFailed() {
         // Given
         let view = MessageReadIndicatorView(
             readUsers: [],
             showReadCount: false,
             localState: .sendingFailed
+        )
+        .frame(width: 50, height: 16)
+
+        // Then
+        assertSnapshot(matching: view, as: .image(perceptualPrecision: precision))
+    }
+
+    func test_messageReadIndicatorView_snapshotMessageEditingFailed() {
+        // Given
+        let view = MessageReadIndicatorView(
+            readUsers: [],
+            showReadCount: false,
+            localState: .syncingFailed
         )
         .frame(width: 50, height: 16)
 
