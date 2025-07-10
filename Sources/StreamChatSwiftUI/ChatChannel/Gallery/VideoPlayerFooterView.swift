c5440b29085fa71cb41be12cd7844927a25ae938
schornon <45765579+schornon@users.noreply.github.com>
2025-07-10 16:10:10 +0300

    Add a view factory method to customise the footer in VideoPlayerView (#871)

---
+++ Sources/StreamChatSwiftUI/ChatChannel/Gallery/VideoPlayerFooterView.swift
@@ -0,0 +1,25 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+import SwiftUI
+
+/// View used as a video player's footer.
+struct VideoPlayerFooterView: View {
+
+    @Injected(\.colors) private var colors
+
+    let attachment: ChatMessageVideoAttachment
+    @Binding var shown: Bool
+
+    var body: some View {
+        HStack {
+            ShareButtonView(content: [attachment.payload.videoURL])
+                .standardPadding()
+
+            Spacer()
+        }
+        .foregroundColor(Color(colors.text))
+    }
+}
