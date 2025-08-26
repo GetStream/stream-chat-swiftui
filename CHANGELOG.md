b11039ba84586cba10b8d3184931b0df61db6258
Toomas Vahter <toomas.vahter@getstream.io>
2025-08-26 12:12:24 +0300

    Show attachment title instead of URL in the file attachment preview view

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 # Upcoming
 
-### 🔄 Changed
+### 🐞 Fixed
+- Show attachment title instead of URL in the `FileAttachmentPreview` view [#930](https://github.com/GetStream/stream-chat-swiftui/pull/930)
 
 # [4.86.0](https://github.com/GetStream/stream-chat-swiftui/releases/tag/4.86.0)
 _August 21, 2025_
---
+++ StreamChatSwiftUITests/Tests/ChatChannel/ChannelInfo/FileAttachmentPreview_Tests.swift
@@ -0,0 +1,20 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+@testable import SnapshotTesting
+@testable import StreamChatSwiftUI
+@testable import StreamChatTestTools
+import SwiftUI
+import XCTest
+
+class FileAttachmentPreview_Tests: StreamChatTestCase {
+    func test_fileAttachmentPreview_pdf() {
+        let view = FileAttachmentPreview(
+            title: "Document title",
+            url: URL.localYodaQuote
+        ).applyDefaultSize()
+        
+        assertSnapshot(matching: view, as: .image(perceptualPrecision: precision))
+    }
+}
