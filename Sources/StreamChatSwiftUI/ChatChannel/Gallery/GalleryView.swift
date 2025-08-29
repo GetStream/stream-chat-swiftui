f25b9ec25e9c16ba62acf03e88b1e474b6875b17
Lucas Nguyen <quang.nguyen@deputy.com>
2025-08-29 20:51:12 +0700

    Fix the video attachments now fetch the URL once on appear and pause when swiping to another item in the gallery. (#934)
    * Fix the video attachments now fetch the URL once on appear and pause when swiping to another item in the gallery.
    
    * Resume playback once the video appears

--- Sources/StreamChatSwiftUI/ChatChannel/Gallery/GalleryView.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/Gallery/GalleryView.swift
@@ -168,6 +168,10 @@ struct StreamVideoPlayer: View {
             }
         }
         .onAppear {
+            guard avPlayer == nil else {
+                avPlayer?.play()
+                return
+            }
             fileCDN.adjustedURL(for: url) { result in
                 switch result {
                 case let .success(url):
@@ -178,6 +182,9 @@ struct StreamVideoPlayer: View {
                     self.error = error
                 }
             }
+        }
+        .onDisappear {
+            avPlayer?.pause()
         }
     }
 }
