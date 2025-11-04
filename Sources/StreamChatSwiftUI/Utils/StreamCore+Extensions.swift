bb5cc83b69dd104c9abf024dd07cbd4a6d19fc0b
Toomas Vahter <toomas.vahter@getstream.io>
2025-11-04 15:56:55 +0200

    Test typealias over @_exported

--- Sources/StreamChatSwiftUI/Utils/StreamCore+Extensions.swift
+++ Sources/StreamChatSwiftUI/Utils/StreamCore+Extensions.swift
@@ -2,4 +2,9 @@
 // Copyright © 2025 Stream.io Inc. All rights reserved.
 //
 
-@_exported import StreamCore
+import Foundation
+import StreamCore
+
+typealias Injected<T> = StreamCore.Injected<T>
+typealias InjectedValues = StreamCore.InjectedValues
+typealias StreamConcurrency = StreamCore.StreamConcurrency
