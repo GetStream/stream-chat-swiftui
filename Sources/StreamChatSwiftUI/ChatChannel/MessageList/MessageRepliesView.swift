c70124d4038fc86067ceb097e3e5b260af4ff003
Nuno Vieira <nuno.fcvieira93@gmail.com>
2025-12-12 20:50:35 +0000

    Fix E2E Tests

--- Sources/StreamChatSwiftUI/ChatChannel/MessageList/MessageRepliesView.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/MessageList/MessageRepliesView.swift
@@ -60,28 +60,12 @@ public struct MessageRepliesView<Factory: ViewFactory>: View {
         } label: {
             HStack {
                 if !isRightAligned {
-                    if let participant = message.threadParticipants.first {
-                        let displayInfo = UserDisplayInfo(
-                            id: participant.id,
-                            name: participant.name ?? participant.id,
-                            imageURL: participant.imageURL,
-                            size: .init(width: 16, height: 16)
-                        )
-                        factory.makeMessageAvatarView(for: displayInfo)
-                    }
+                    messageAvatarView
                 }
                 Text(title)
                     .font(fonts.footnoteBold)
                 if isRightAligned {
-                    if let participant = message.threadParticipants.first {
-                        let displayInfo = UserDisplayInfo(
-                            id: participant.id,
-                            name: participant.name ?? participant.id,
-                            imageURL: participant.imageURL,
-                            size: .init(width: 16, height: 16)
-                        )
-                        factory.makeMessageAvatarView(for: displayInfo)
-                    }
+                    messageAvatarView
                 }
             }
             .padding(.horizontal, 16)
@@ -130,6 +114,28 @@ public struct MessageRepliesView<Factory: ViewFactory>: View {
             return L10n.Message.Threads.reply
         } else {
             return L10n.Message.Threads.replies
+        }
+    }
+
+    private var messageAvatarView: some View {
+        // This is just a fallback for backwards compatibility
+        // In practice thread participants will never be empty.
+        // So, the factory method will always run.
+        Group {
+            if let participant = message.threadParticipants.first {
+                let displayInfo = UserDisplayInfo(
+                    id: participant.id,
+                    name: participant.name ?? participant.id,
+                    imageURL: participant.imageURL,
+                    size: .init(width: 16, height: 16)
+                )
+                factory.makeMessageAvatarView(for: displayInfo)
+            } else {
+                MessageAvatarView(
+                    avatarURL: message.threadParticipants.first?.imageURL,
+                    size: .init(width: 16, height: 16)
+                )
+            }
         }
     }
 }
