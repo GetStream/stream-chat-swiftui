0329563053978daf6c1f5a10b2f0d98d933622e7
Alexey Alter-Pesotskiy <alex@testableapple.com>
2025-12-16 17:08:32 +0000

    [CI] Implement new mock server

--- Sources/StreamChatSwiftUI/ViewFactory/DefaultViewFactory.swift
+++ Sources/StreamChatSwiftUI/ViewFactory/DefaultViewFactory.swift
@@ -835,13 +835,13 @@ extension ViewFactory {
     }
     
     public func makeQuotedMessageContentView(
-            options: QuotedMessageContentViewOptions
-        ) -> some View {
-            QuotedMessageContentView(
-                factory: self,
-                options: options
-            )
-        }
+        options: QuotedMessageContentViewOptions
+    ) -> some View {
+        QuotedMessageContentView(
+            factory: self,
+            options: options
+        )
+    }
     
     public func makeCustomAttachmentQuotedView(options: CustomAttachmentQuotedViewOptions) -> some View {
         EmptyView()
