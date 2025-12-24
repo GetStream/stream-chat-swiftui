8dc9a7687498ecb90b1fa42d67edba404265ddfa
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2025-12-24 16:16:38 +0100

    Use extensions from common ui

--- Sources/StreamChatSwiftUI/ChatChannel/Composer/PhotoAttachmentPickerView.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/Composer/PhotoAttachmentPickerView.swift
@@ -3,6 +3,7 @@
 //
 
 import Photos
+import StreamChatCommonUI
 import SwiftUI
 
 /// View for the photo attachment picker.
@@ -220,5 +221,17 @@ public struct PhotoAttachmentCell: View {
             return selectedAssetIds.contains(id)
         }
         return imageSelected(id)
+    }
+}
+
+extension UIImage {
+    func saveAsJpgToTemporaryUrl() throws -> URL? {
+        guard let imageData = jpegData(compressionQuality: 1.0) else { return nil }
+        let imageName = "\(UUID().uuidString).jpg"
+        let documentDirectory = NSTemporaryDirectory()
+        let localPath = documentDirectory.appending(imageName)
+        let photoURL = URL(fileURLWithPath: localPath)
+        try imageData.write(to: photoURL)
+        return photoURL
     }
 }
