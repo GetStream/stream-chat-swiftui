a013f8fd8ba96c9d016f8624d41a205055f0af52
Alexey Alter-Pesotskiy <alex@testableapple.com>
2026-01-27 18:16:01 +0000

    Update StreamChat dependency to 4.97.0 (#1149)

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "4.96.0")
+        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "4.97.0")
     ],
     targets: [
         .target(
