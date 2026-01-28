0940a6a31e6389ea4b863e147d1fea67fbbc5484
Toomas Vahter <toomas.vahter@getstream.io>
2026-01-28 14:33:25 +0200

    Update reaction overlays

--- Sources/StreamChatSwiftUI/ChatChannel/Reactions/ReactionsBubbleView.swift
+++ Sources/StreamChatSwiftUI/ChatChannel/Reactions/ReactionsBubbleView.swift
@@ -8,20 +8,19 @@ import SwiftUI
 /// Modifier that enables message bubble container.
 public struct ReactionsBubbleModifier: ViewModifier {
     @Injected(\.colors) private var colors
+    @Injected(\.tokens) private var tokens
 
     var message: ChatMessage
 
     var borderColor: Color?
     var injectedBackground: UIColor?
 
-    private let cornerRadius: CGFloat = 18
-
     public func body(content: Content) -> some View {
         content
             .background(Color(backgroundColor))
             .overlay(
                 BubbleBackgroundShape(
-                    cornerRadius: cornerRadius, corners: corners
+                    cornerRadius: tokens.radiusMax, corners: corners
                 )
                 .stroke(
                     borderColor ?? Color(colors.reactionBorder),
@@ -30,7 +29,7 @@ public struct ReactionsBubbleModifier: ViewModifier {
             )
             .clipShape(
                 BubbleBackgroundShape(
-                    cornerRadius: cornerRadius,
+                    cornerRadius: tokens.radiusMax,
                     corners: corners
                 )
             )
