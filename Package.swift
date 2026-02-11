cd62394b219bc39b69b18f722a8538c6da81e767
Alexey Alter-Pesotskiy <alex@testableapple.com>
2026-02-11 16:48:53 +0000

    Update StreamChat dependency to 4.97.1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "4.97.0")
+        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "4.97.1")
     ],
     targets: [
         .target(
--- StreamChatSwiftUI.podspec
+++ StreamChatSwiftUI.podspec
@@ -19,5 +19,5 @@ Pod::Spec.new do |spec|
 
   spec.framework = 'Foundation', 'UIKit', 'SwiftUI'
 
-  spec.dependency 'StreamChat', '~> 4.97.0'
+  spec.dependency 'StreamChat', '~> 4.97.1'
 end
