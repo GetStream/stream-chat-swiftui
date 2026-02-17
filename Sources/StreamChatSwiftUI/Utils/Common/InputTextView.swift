265167ea55e243eeef324ccc136259dd5459a397
Alexey Alter-Pesotskiy <alex@testableapple.com>
2026-02-17 14:12:07 +0000

    Revert "Apply coderabbit suggestion"
    This reverts commit 56f89c982b4745a342c1baef14bc062cad55b8bc.

--- Sources/StreamChatSwiftUI/Utils/Common/InputTextView.swift
+++ Sources/StreamChatSwiftUI/Utils/Common/InputTextView.swift
@@ -118,6 +118,7 @@ class InputTextView: UITextView, AccessibilityView {
         NSLayoutConstraint.activate([
             placeholderLabel.leadingAnchor.pin(equalTo: leadingAnchor, constant: directionalLayoutMargins.leading),
             placeholderLabel.trailingAnchor.pin(equalTo: trailingAnchor, constant: -directionalLayoutMargins.trailing),
+            placeholderLabel.widthAnchor.pin(equalTo: layoutMarginsGuide.widthAnchor),
             placeholderLabel.topAnchor.pin(equalTo: topAnchor),
             placeholderLabel.bottomAnchor.pin(lessThanOrEqualTo: bottomAnchor),
             placeholderLabel.centerYAnchor.pin(equalTo: centerYAnchor)
