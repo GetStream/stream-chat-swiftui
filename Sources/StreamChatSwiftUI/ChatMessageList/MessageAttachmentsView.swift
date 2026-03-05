cd98c27d1cc7fd082fd1caf425d0b2c91d56735e
Toomas Vahter <toomas.vahter@getstream.io>
2026-03-05 10:37:42 +0200

    Fix attachment alignment

--- Sources/StreamChatSwiftUI/ChatMessageList/MessageAttachmentsView.swift
+++ Sources/StreamChatSwiftUI/ChatMessageList/MessageAttachmentsView.swift
@@ -56,70 +56,68 @@ public struct MessageAttachmentsView<Factory: ViewFactory>: View {
     }
 
     public var body: some View {
-        VStack(
-            alignment: message.alignmentInBubble,
-            spacing: tokens.spacingXs
-        ) {
-            if let quotedMessage = message.quotedMessage {
-                factory.makeChatQuotedMessageView(
-                    options: ChatQuotedMessageViewOptions(
-                        quotedMessage: quotedMessage,
-                        parentMessage: message,
-                        availableWidth: width,
-                        scrolledId: $scrolledId
+        VStack(alignment: message.alignmentInBubble, spacing: tokens.spacingXs) {
+            VStack(alignment: message.isRightAligned ? .trailing : .leading, spacing: tokens.spacingXs) {
+                if let quotedMessage = message.quotedMessage {
+                    factory.makeChatQuotedMessageView(
+                        options: ChatQuotedMessageViewOptions(
+                            quotedMessage: quotedMessage,
+                            parentMessage: message,
+                            availableWidth: width,
+                            scrolledId: $scrolledId
+                        )
                     )
-                )
-            }
-
-            // Media (images and/or videos)
-            if messageTypeResolver.hasImageAttachment(message: message) || messageTypeResolver.hasVideoAttachment(message: message) {
-                factory.makeMediaAttachmentsView(
-                    options: MediaAttachmentsViewOptions(
-                        message: message,
-                        isFirst: isFirst,
-                        availableWidth: width,
-                        scrolledId: $scrolledId
+                }
+                
+                // Media (images and/or videos)
+                if messageTypeResolver.hasImageAttachment(message: message) || messageTypeResolver.hasVideoAttachment(message: message) {
+                    factory.makeMediaAttachmentsView(
+                        options: MediaAttachmentsViewOptions(
+                            message: message,
+                            isFirst: isFirst,
+                            availableWidth: width,
+                            scrolledId: $scrolledId
+                        )
                     )
-                )
-            }
-
-            // Files
-            if messageTypeResolver.hasFileAttachment(message: message) {
-                factory.makeFileAttachmentView(
-                    options: FileAttachmentViewOptions(
-                        message: message,
-                        isFirst: isFirst,
-                        availableWidth: width,
-                        scrolledId: $scrolledId
+                }
+                
+                // Files
+                if messageTypeResolver.hasFileAttachment(message: message) {
+                    factory.makeFileAttachmentView(
+                        options: FileAttachmentViewOptions(
+                            message: message,
+                            isFirst: isFirst,
+                            availableWidth: width,
+                            scrolledId: $scrolledId
+                        )
                     )
-                )
-            }
-
-            // Voice recordings
-            if messageTypeResolver.hasVoiceRecording(message: message) {
-                factory.makeVoiceRecordingView(
-                    options: VoiceRecordingViewOptions(
-                        message: message,
-                        isFirst: isFirst,
-                        availableWidth: width,
-                        scrolledId: $scrolledId
+                }
+                
+                // Voice recordings
+                if messageTypeResolver.hasVoiceRecording(message: message) {
+                    factory.makeVoiceRecordingView(
+                        options: VoiceRecordingViewOptions(
+                            message: message,
+                            isFirst: isFirst,
+                            availableWidth: width,
+                            scrolledId: $scrolledId
+                        )
                     )
-                )
-            }
-
-            // Link previews
-            if messageTypeResolver.hasLinkAttachment(message: message)
-                && message.attachmentCounts.keys.allSatisfy({ $0 == .linkPreview }) {
-                factory.makeLinkAttachmentView(
-                    options: LinkAttachmentViewOptions(
-                        message: message,
-                        isFirst: isFirst,
-                        availableWidth: width,
-                        scrolledId: $scrolledId
+                }
+                
+                // Link previews
+                if messageTypeResolver.hasLinkAttachment(message: message)
+                    && message.attachmentCounts.keys.allSatisfy({ $0 == .linkPreview }) {
+                    factory.makeLinkAttachmentView(
+                        options: LinkAttachmentViewOptions(
+                            message: message,
+                            isFirst: isFirst,
+                            availableWidth: width,
+                            scrolledId: $scrolledId
+                        )
                     )
-                )
+                }
             }
-
             // Text caption
             if !message.text.isEmpty {
                 AttachmentTextView(factory: factory, message: message)
