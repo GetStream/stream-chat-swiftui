3a5f094a18de2836537b481bf10ddbe053c67cd7
Toomas Vahter <toomas.vahter@getstream.io>
2026-06-11 13:06:11 +0300

    Update StreamChat dependency to 5.5.1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "5.5.0")
+        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "5.5.1")
     ],
     targets: [
         .target(
