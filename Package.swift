9badcf5d3f8c2c349de2a140e9548d47e279ab75
Nuno Vieira <nuno.fcvieira93@gmail.com>
2026-07-23 16:02:21 +0100

    Update StreamChat dependency to 4.102.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "4.101.1")
+        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "4.102.0")
     ],
     targets: [
         .target(
