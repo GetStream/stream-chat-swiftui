4ca185a6b9b9f567d105e82f39e7fbc0aecdc1ea
Nuno Vieira <nuno.fcvieira93@gmail.com>
2026-08-13 18:34:34 +0100

    Pin StreamChat to merged mention suggestions commit
    Use develop revision 8263bddc2 now that #4213 is merged.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/GetStream/stream-chat-swift.git", branch: "fix/mention-suggestions-large-channels")
+        .package(url: "https://github.com/GetStream/stream-chat-swift.git", revision: "8263bddc2f9395fb1c4a275c86db711ceda954ed")
     ],
     targets: [
         .target(
