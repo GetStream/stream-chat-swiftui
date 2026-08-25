54701df74dae42fd68940ad84ed4eff23918b587
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2026-08-25 09:04:37 +0200

    Support older Xcode versions

--- Sources/StreamChatSwiftUI/Utils/Common/Compatibility+ScrollEdgeEffect.swift
+++ Sources/StreamChatSwiftUI/Utils/Common/Compatibility+ScrollEdgeEffect.swift
@@ -7,7 +7,7 @@ import SwiftUI
 extension Compatibility where Content: View {
     @ViewBuilder
     func scrollEdgeEffectHidden(_ hidden: Bool, for edges: Edge.Set) -> some View {
-        #if os(iOS)
+        #if os(iOS) && compiler(>=6.3)
         if #available(iOS 27.0, *) {
             content.scrollEdgeEffectHidden(hidden, for: edges)
         } else {
