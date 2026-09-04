7f8d724db81bbe4330f0b07820483e4ec776473c
Nuno Vieira <nuno.fcvieira93@gmail.com>
2026-09-04 12:42:43 +0100

    Point StreamChat dependency at the IOS-2005 LLC branch
    Temporarily pin SPM to fix/ios-2005-edit-message-mentions so the SwiftUI demo can use the new editMessage mention APIs for testing.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/GetStream/stream-chat-swift.git", from: "5.10.0")
+        .package(url: "https://github.com/GetStream/stream-chat-swift.git", branch: "fix/ios-2005-edit-message-mentions")
     ],
     targets: [
         .target(
